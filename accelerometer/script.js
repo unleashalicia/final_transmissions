eb6438a70dd0768f6f6377470deaf4ce6a9bfe81
Finleth <jeffrey@paufamily.com>
2017-12-15 18:15:46 -0800

    un-commented out the alpha, beta, and gamma angles and their console.logs

--- accelerometer/script.js
+++ accelerometer/script.js
@@ -3,12 +3,12 @@ window.addEventListener("devicemotion", handleMotion, true);
 
 function handleMotion(DeviceMotionEvent) {
     var acceleration = DeviceMotionEvent.acceleration;
-    // var alpha    = DeviceMotionEvent.alpha;
-    // var beta     = DeviceMotionEvent.beta;
-    // var gamma    = DeviceMotionEvent.gamma;
+    var alpha    = DeviceMotionEvent.alpha;
+    var beta     = DeviceMotionEvent.beta;
+    var gamma    = DeviceMotionEvent.gamma;
 
     console.log("value pertaining to acceleration: ", acceleration);
-    // console.log("value pertaining to alpha: ", alpha);
-    // console.log("value pertaining to beta: ", beta);
-    // console.log("value pertaining to gamma: ", gamma);
+    console.log("value pertaining to alpha: ", alpha);
+    console.log("value pertaining to beta: ", beta);
+    console.log("value pertaining to gamma: ", gamma);
 }
