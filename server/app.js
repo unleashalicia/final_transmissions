694c4631ce9d598edd67ae8c051e8722332ace82
Cesca <csinocruz@gmail.com>
2017-12-27 14:40:52 -0800

    made a few changes with Drew -tutoring

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
+const path = require("path");
 const server = express();
 const PORT = 8000;
 
@@ -9,10 +10,21 @@ server.use(logger('dev')); //other options exist other than dev, look into it
 server.use(express.json()); //takes the place of...
 server.use(express.urlencoded()); //...body-parser
 
+
+server.use(express.static(path.join(__dirname, '..', 'client')));
 server.get('/',(req,res) => {
-    res.send('Booo!');
+    res.sendFile(path.join(__dirname, '..', 'client/index.html'))
+})
+
+server.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client/signup.html'))
+    //console.log(path.join(__dirname, '..', 'client/index.html'));
+    //res.sendFile(path.join(__dirname, '..', 'client/index.html'))
 })
 
 server.listen(PORT, () => {
     console.log("Let's find some ghosts on port: ", PORT);
 })
+
+
+//mongoose schema resources mongolabs mongo university  
