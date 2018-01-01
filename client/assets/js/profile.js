d0d385bf66dd04679231eb6051edad22f2b66dc1
Francis Redido <sicnarfodider@hotmail.com>
2017-12-31 18:17:31 -0800

    tested a new to display stories, show it in a list then have it become a drop down

--- client/assets/js/profile.js
+++ client/assets/js/profile.js
@@ -8,36 +8,50 @@
 
 
 function handleEventHandlers(){
-  var profile = document.querySelectorAll(".profile")[0];
-  var stories = document.querySelectorAll(".stories")[0];
-  var profileContainer = document.querySelectorAll(".profile-container")[0];
-  var story = document.getElementsByClassName("story");
-  Array.from(story).forEach(elem => elem.addEventListener('click', function() {
-  this.children[0].classList.add("slideDown")
-  }));
-  profile.addEventListener("click", function(){   profileContainer.classList.toggle("hide")});
-  stories.addEventListener("click", function(){ profileContainer.classList.add("hide")});
-  var edit = document.getElementsByClassName("edit");
-  var account = document.querySelectorAll(".account-settings")[0];
-  debugger;
-  Array.from(edit).forEach(elem => elem.addEventListener('click', function() {
-  account.classList.remove("hide")
-  }));
-  var submitBtn = document.querySelectorAll(".submit")[0];
-  submitBtn.addEventListener("click",handleSubmit)
+    var profile = document.querySelectorAll(".profile")[0];
+    var stories = document.querySelectorAll(".stories")[0];
+    var profileContainer = document.querySelectorAll(".profile-container")[0];
+    var account = document.querySelectorAll(".account-settings")[0];
+    var edit = document.getElementsByClassName("edit");
+    var story = document.getElementsByClassName("story");
+    var submitBtn = document.querySelectorAll(".submit")[0];
+    var cancelBtn = document.querySelectorAll(".cancel")[0];
+    var storylist = document.querySelectorAll(".story-list")[0];
+    Array.from(story).forEach(elem => elem.addEventListener('click', function() {
+    this.children[0].classList.add("slideDown")
+    }));
+    profile.addEventListener("click", function(){   profileContainer.classList.toggle("hide")});
+    stories.addEventListener("click", function(){ profileContainer.classList.add("hide")});
+    Array.from(edit).forEach(elem => elem.addEventListener('click', function() {
+    account.classList.remove("hide")
+    }));
+    submitBtn.addEventListener("click",handleSubmit);
+    cancelBtn.addEventListener("click",handleCancel);
+    storylist.addEventListener("click",showStory);
+
 }
 
+function showStory(){
+  this.firstElementChild.classList.remove("hide");
+  this.firstElementChild.classList.add("slideDown");
+  console.log(this)
+}
 
 function handleSubmit(){
-  event.preventDefault();
-  var usernameInput = document.querySelector(".username").value;
-  var emailInput= document.querySelector(".email").value;
-  var account = document.querySelectorAll(".account-settings")[0];
-  account.classList.add("hide")
-  console.log(".username", usernameInput, "& .email" , emailInput);
-
+    event.preventDefault();
+    var usernameInput = document.querySelector(".username").value;
+    var emailInput= document.querySelector(".email").value;
+    var account = document.querySelectorAll(".account-settings")[0];
+    account.classList.add("hide")
+    console.log(".username", usernameInput, "& .email" , emailInput);
+    document.querySelector(".username").value="";
+    document.querySelector(".email").value="";
 }
 
 function handleCancel(){
-
+  event.preventDefault();
+   document.querySelector(".username").value="";
+   document.querySelector(".email").value="";
+   var account = document.querySelectorAll(".account-settings")[0];
+   account.classList.add("hide")
 }
