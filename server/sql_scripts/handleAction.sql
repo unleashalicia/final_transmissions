5d4b5ad8f90b373b8d66d73d878eaab1ae6ed117
Finleth <jeffrey@paufamily.com>
2018-01-04 14:45:05 -0800

    finally got two queries in my stored procedure without it throwing an error and it works toooooo

--- server/sql_scripts/handleAction.sql
+++ server/sql_scripts/handleAction.sql
@@ -4,17 +4,19 @@ DELIMITER $$
 
 DROP PROCEDURE IF EXISTS `handleUserAction` $$
 CREATE PROCEDURE `handleUserAction` (
-    BEGIN
     IN `userId` INT,
     IN `user_action` VARCHAR(15)
-    )  NO SQL
+  )  MODIFIES SQL DATA
 
+  BEGIN
+    SELECT sa.next_state_id FROM users AS u
+      JOIN state_actions AS sa
+      ON sa.state_id = u.state_id
+      WHERE userId = u.ID AND user_action = sa.action
+    ;
+      SELECT * from users
+    ;
+  END
+    $$
 
-  SELECT sa.next_state_id FROM states AS s
-	JOIN users AS u
-    ON u.state_id = s.state_id
-  JOIN state_actions AS sa
-    ON sa.state_id = s.state_id
-    WHERE userId = u.ID AND user_action = sa.action
-
-    END $$
+DELIMITER ;
