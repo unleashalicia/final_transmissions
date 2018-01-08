7d9feb036929a7ada948fa1d54531ed7bdf2b614
sloumdrone <brian.michael.evans@gmail.com>
2018-01-08 13:16:19 -0800

    Added a path for the meter

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const logger = require('morgan'); 
+const logger = require('morgan');
 const passport = require('passport');
 const session = require('express-session');
 const path = require('path');
@@ -7,13 +7,13 @@ const path = require('path');
 const app = express();
 const PORT = 8000;
 
-app.use(logger('dev')); 
-app.use(express.json()); 
-app.use(express.urlencoded()); 
-app.use('/assets', express.static(path.join(__dirname, '..', 'client', 'assets'))); 
-app.use(session({ secret: 'wishbone' })); 
-app.use(passport.initialize()); 
-app.use(passport.session()); 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use('/assets', express.static(path.join(__dirname, '..', 'client', 'assets')));
+app.use(session({ secret: 'wishbone' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 const {passportMethod, connection} = require('./passport');
 
@@ -38,6 +38,10 @@ app.get('/login', (req, res) => {
 
 app.get('/profile', (req, res) => {
     res.sendFile(path.join(__dirname,'..', 'client', 'profile.html'));
+});
+
+app.get('/play', (req, res) => {
+    res.sendFile(path.join(__dirname,'..', 'client', 'meter-index.html'));
 });
 
 
@@ -66,7 +70,7 @@ app.post('/action', (req, res) => {
 
 
 
-function errorHandler (err, req, res, next) { 
+function errorHandler (err, req, res, next) {
 	if (res.headersSent) {
 	  return next(err);
 	}
@@ -77,4 +81,3 @@ function errorHandler (err, req, res, next) {
 app.listen(PORT, () => {
     console.log("Let's find some ghosts on port: ", PORT);
 });
-
