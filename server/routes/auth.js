3e9a9d738a135771a979ca3ab09dd7d4c5e2ec70
sloumdrone <brian.michael.evans@gmail.com>
2018-01-09 16:26:21 -0800

    Test

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,6 @@
 const path = require('path');
+var fs = require('fs');
+
 
 module.exports = function (app, passport) {
 
@@ -22,6 +24,8 @@ module.exports = function (app, passport) {
 	});
 
 	app.get('/profile', isLoggedIn, (req, res) => {
+		// fs.writeFile("./reqInfo",req
+		let user = req.user.sessionID
 	    res.render("profile",{
 			data: 'This is a test'
 		});
