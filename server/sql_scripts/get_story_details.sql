b008dcedd49ec6de7d9609ce4654e306dd192405
Finleth <jeffrey@paufamily.com>
2018-01-17 19:35:43 -0800

    updated the stored procedure for getting the db assets for our story page to match our database

--- server/sql_scripts/get_story_details.sql
+++ server/sql_scripts/get_story_details.sql
@@ -4,16 +4,13 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS `getStoryPageDetails`
     $$
 CREATE PROCEDURE `getStoryPageDetails` (
-    IN `userId` SMALLINT(5), 
     IN `storyId` TINYINT(3))  
     
     BEGIN
         SELECT sd.*, us.state_id, s.name, s.story_img FROM story_details AS sd
-        JOIN user_stories AS us
-        ON us.story_id = sd.story_id
         JOIN stories AS s
         ON s.id = sd.story_id
-        WHERE sd.story_id = storyId AND us.id = userId
+        WHERE sd.story_id = storyId
         ;
         SELECT state_details AS chapter_name, state_id FROM states
         WHERE story_id = storyId
