44fef2b9b54da7465996ba3d75598975daf3cac8
Finleth <jeffrey@paufamily.com>
2018-01-18 16:07:38 -0800

    updated the sql script for our get state stored procedure so it now includes the insert statement for inserting a user's progress into our user_stories table if they don't exist in it

--- server/sql_scripts/get_state_details.sql
+++ server/sql_scripts/get_state_details.sql
@@ -9,10 +9,13 @@ CREATE PROCEDURE `getUserStateDetails` (
   )  READS SQL DATA
 
     BEGIN
-      SELECT ar.marker,
-            l.latitude AS lat, l.longitude AS lon, l.outer_threshold, l.inner_threshold,
-            sa.action, us.state_id
-
+        IF NOT EXISTS (SELECT state_id FROM user_stories WHERE id = userId AND story_id = storyId)
+        THEN INSERT INTO user_stories (`id`, `story_id`)	
+        VALUES (userId, storyId)
+        ;    
+        END IF ;
+        
+        SELECT ar.marker, l.latitude AS lat, l.longitude AS lon, l.outer_threshold, l.inner_threshold, sa.action, us.state_id
         FROM user_stories AS us
 
         JOIN location AS l
@@ -24,8 +27,8 @@ CREATE PROCEDURE `getUserStateDetails` (
 
         WHERE us.id = userId AND us.story_id = storyId
         ;
-        
-      SELECT  outer_audio_url AS outerAudio, inner_audio_url AS innerAudio, completed_audio_url AS completeAudio
+            
+        SELECT  outer_audio_url AS outerAudio, inner_audio_url AS innerAudio, completed_audio_url AS completeAudio
         FROM user_stories AS us
         JOIN audio AS a
         ON a.state_id = us.state_id
