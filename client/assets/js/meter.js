b259f2de78a738baf13dbbbf9f52d563e8956339
sloumdrone <brian.michael.evans@gmail.com>
2018-01-21 14:09:57 -0800

    Adding error logging for axios failure and adding audio files

--- client/assets/js/meter.js
+++ client/assets/js/meter.js
@@ -421,6 +421,8 @@ function grabChapterAssets(){
     const storyID = sessionStorage.getItem('story_id');
 
     axios.get('/state',{params : {story: storyID}}).then( handleStateAssetLoading ).catch( error => {
+        console.warn('Axios GET from state issue');
+        console.log(error);
         window.location.href = "/story/id/" + storyID;
     });
 }
